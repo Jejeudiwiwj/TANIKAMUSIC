dd5a7101e20b224d83aa054e33be5b094e5a362c
Jejeudiwiwj <92663147+Jejeudiwiwj@users.noreply.github.com>
2024-01-07 16:54:40 +0530

    Add files via upload

---
+++ VIPMUSIC/plugins/tools/zowner.py
@@ -0,0 +1,62 @@
+from pyrogram import Client, filters
+import requests
+import random
+import os
+import re
+import asyncio
+import time
+from VIPMUSIC import app
+
+from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
+
+
+app.on_message(
+    filters.command("repo")
+    & filters.group)
+async def help(client: Client, message: Message):
+    await message.reply_photo(
+        photo=f"https://telegra.ph/file/9d9489e4b0484b6ea8908.jpg",
+        caption=f"""🍁𝐂𝐋𝐈𝐂𝐊🥰𝐁𝐄𝐋𝐎𝐖💝𝐁𝐔𝐓𝐓𝐎𝐍✨𝐓𝐎🙊𝐆𝐄𝐓🌱𝐑𝐄𝐏𝐎🍁""",
+        reply_markup=InlineKeyboardMarkup(
+            [
+                [
+                    InlineKeyboardButton(
+                        "🌱ƨσʋяcɛ🌱", url=f"https://telegra.ph/file/c224f14af80f3cd285220.jpg")
+                ]
+            ]
+        ),
+    )
+
+@app.on_message(
+    filters.command("repo")
+    & filters.group)
+async def help(client: Client, message: Message):
+    await message.reply_photo(
+        photo=f"https://telegra.ph/file/9d9489e4b0484b6ea8908.jpg",
+        caption=f"""🍁𝐂𝐋𝐈𝐂𝐊🥰𝐁𝐄𝐋𝐎𝐖💝𝐁𝐔𝐓𝐓𝐎𝐍✨𝐓𝐎🙊𝐆𝐄𝐓🌱𝐑𝐄𝐏𝐎🍁""",
+        reply_markup=InlineKeyboardMarkup(
+            [
+                [
+                    InlineKeyboardButton(
+                        "🌱ƨσʋяcɛ🌱", url=f"https://telegra.ph/file/c224f14af80f3cd285220.jpg")
+                ]
+            ]
+        ),
+    )
+
+@app.on_message(
+    filters.command("repo")
+    & filters.private)
+async def help(client: Client, message: Message):
+    await message.reply_photo(
+        photo=f"https://telegra.ph/file/9d9489e4b0484b6ea8908.jpg",
+        caption=f"""🍁𝐂𝐋𝐈𝐂𝐊🥰𝐁𝐄𝐋𝐎𝐖💝𝐁𝐔𝐓𝐓𝐎𝐍✨𝐓𝐎🙊𝐆𝐄𝐓🌱𝐑𝐄𝐏𝐎🍁""",
+        reply_markup=InlineKeyboardMarkup(
+            [
+                [
+                    InlineKeyboardButton(
+                        "🌱ƨσʋяcɛ🌱", url=f"https://telegra.ph/file/c224f14af80f3cd285220.jpg")
+                ]
+            ]
+        ),
+    )
